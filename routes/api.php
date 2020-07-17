18c7cddb264feecb6888c7d8db833caaf9042f42
Paul Cortes <paulkerg@gmail.com>
2020-07-16 21:38:03 -0500

    Limpieza de archivo de rutas

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,6 @@
 <?php
 
 use CloudCreativity\LaravelJsonApi\Facades\JsonApi;
-use Illuminate\Support\Facades\Route;
-
-//Route::get('articles/{article}', 'ArticleController@show')->name('api.v1.articles.read');
-//Route::get('articles', 'ArticleController@index')->name('api.v1.articles.index');
 
 JsonApi::register('v1')->routes(function($api){
     $api->resource('articles');
