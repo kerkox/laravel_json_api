357b9307cd5d10b96aa614135785fbdcbfc40ea3
Paul Cortes <paulkerg@gmail.com>
2020-08-17 18:20:28 -0500

    Prueba de listado de autores

--- tests/Feature/Authors/ListAuthorsTest.php
+++ tests/Feature/Authors/ListAuthorsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Authors;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -11,7 +12,7 @@ class ListAuthorsTest extends TestCase
     /** @test */
     public function can_fetch_all_authors()
     {
-       $authors = factory(USer::class)->times(3)->create();
+       $authors = factory(User::class)->times(3)->create();
 
        $this->jsonApi()->get(route('api.v1.authors.index'))
            ->assertSee($authors[0]->name);
